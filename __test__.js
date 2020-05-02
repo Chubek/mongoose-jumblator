375dff1106c31fd4bf4cc0c5ab62be81deb5f76c
chubek <aurlito@riseup.net>
2020-05-02 22:25:50 +0430

    soem progress

--- __test__.js
+++ __test__.js
@@ -1,4 +1,4 @@
-const EncryptField = require("./lib/jumblator-encryption").encrypt;
+/*const EncryptField = require("./lib/jumblator-encryption").encrypt;
 const DecryptField = require("./lib/jumblator-encryption").decrypt;
 
 const plain = "WAR AND PEACE";
@@ -12,3 +12,34 @@ EncryptField(plain, passPhrase)
       .catch((e) => console.error(e));
   })
   .catch((e) => console.error(e));
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jumblator = require("./lib/jumblator-plugin");
+
+mongoose.connect("mongodb://localhost:27017/jumblator-test", {
+  useNewUrlParser: true,
+});
+
+const SCHM = new Schema({
+  hell: {
+    type: String,
+    encrypt: true,
+  },
+});
+
+SCHM.plugin(jumblator, { secret: "HDFDSF", index: true });
+
+const SCHMMODEL = mongoose.model("SSS", SCHM);
+/*
+const schmm = new SCHMMODEL({
+  hell: "HLLL",
+});
+
+schmm.save();
+*/
+
+SCHMMODEL.findOne({ _id: "5eada0e20b7c2c337c8347ea" }).then((doc) =>
+  console.log(doc)
+);
